8a1685922515359d07a1829434ba21e18fd95eb8
maxremus <35401007+maxremus@users.noreply.github.com>
2026-08-07 15:01:25 +0300

    add

--- transport_saas/src/main/java/org/example/transport_saas/entity/Driver.java
+++ transport_saas/src/main/java/org/example/transport_saas/entity/Driver.java
@@ -1,7 +1,16 @@
 package org.example.transport_saas.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "drivers")
@@ -13,7 +22,7 @@ import lombok.*;
 public class Driver {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
